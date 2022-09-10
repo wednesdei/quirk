9176f02af6df677f097bf06a0a17c407fe923ac0
Chris M <44120439+wednesdei@users.noreply.github.com>
2022-09-10 12:38:41 -0600

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:quirk/quirk.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'ui/home/screen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Quirk Canvas'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var color = Colors.blue;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      darkTheme: ThemeData.dark().copyWith(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: const SizedBox.square(
-        dimension: 600,
-        child: Quirk(),
-      ),
+      home: HomeScreen(),
     );
   }
 }
