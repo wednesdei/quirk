792eced9c33d17b795d5d34870e8aea5267d5520
Chris M <44120439+wednesdei@users.noreply.github.com>
2022-09-30 13:42:38 -0600

    updates

---
+++ lib/ui/app/themes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// These are going to be the default [Themes] for the app.
+/// The light and dark versions are completely minimalistic.
+/// [vaporwave] will be the main theme, when its ready :)
+/// [synthwave] will be the secondary theme just for fun :)
+enum ThemeType {
+  light,
+  dark,
+  vaporwave,
+  synthwave,
+}
